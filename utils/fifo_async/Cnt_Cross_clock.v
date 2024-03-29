2815d702f0bf348c7ad39aef0ed39476572d8f99
U-HOPPINGKNIGHT\14817 <1481782745@qq.com>
2024-03-29 11:08:12 +0800

    correct fifo async

--- utils/fifo_async/Cnt_Cross_clock.v
+++ utils/fifo_async/Cnt_Cross_clock.v
@@ -3,13 +3,13 @@
 
 module Cnt_Cross_clock
 #(
-    parameter WIDTH = 8
+  parameter WIDTH = 8
 )
 ( 
-    input                  clk_in     ,
-    input     [WIDTH-1:0]  cnt_in     ,
-    input                  clk_out    ,
-    output    [WIDTH-1:0]  cnt_out
+  input                  clk_in     ,
+  input     [WIDTH-1:0]  cnt_in     ,
+  input                  clk_out    ,
+  output    [WIDTH-1:0]  cnt_out
 );
 
 wire [WIDTH-1:0] cnt_gray;
@@ -23,8 +23,8 @@ assign cnt_gray[WIDTH-2:0] = cnt_in[WIDTH-2:0] ^ cnt_in[WIDTH-1:1];
 // 2. cross clock domain clocking
 
 always@(posedge clk_out) begin
-    cnt_gray_r1 <= cnt_gray;
-    cnt_gray_r2 <= cnt_gray_r1;
+  cnt_gray_r1 <= cnt_gray;
+  cnt_gray_r2 <= cnt_gray_r1;
 end
 
 // 3. decode the gray code to binary
@@ -40,14 +40,14 @@ endmodule
 
 Cnt_Cross_clock
 #(
-    .WIDTH(WIDTH)
+  .WIDTH(WIDTH)
 )
 Cnt_Cross_clock_inst
 ( 
-    .clk_in   (clk_in )  ,
-    .cnt_in   (cnt_in )  ,
-    .clk_out  (clk_out)  ,
-    .cnt_out  (cnt_out)
+  .clk_in   (clk_in )  ,
+  .cnt_in   (cnt_in )  ,
+  .clk_out  (clk_out)  ,
+  .cnt_out  (cnt_out)
 );
 
 */
--- utils/fifo_async/Fifo_Async.v
+++ utils/fifo_async/Fifo_Async.v
@@ -4,28 +4,28 @@
 
 module Fifo_Async
 #(
-    parameter DWI = 8   ,   // data width in
-    parameter AWI = 7   ,   // address width in
-    parameter DWO = 16  ,   // data width out
-    parameter AWO = 6       // address width out
+  parameter DWI = 8   ,   // data width in
+  parameter AWI = 7   ,   // address width in
+  parameter DWO = 16  ,   // data width out
+  parameter AWO = 6       // address width out
 )
 (   
-    input                     rst_n           ,
+  input                     rst_n           ,
 
-    input                     wr_clk          ,
-    input                     wr_en           ,            
-    input       [DWI-1:0]     wr_data         ,
+  input                     wr_clk          ,
+  input                     wr_en           ,            
+  input       [DWI-1:0]     wr_data         ,
 
-    input                     rd_clk          ,
-    input                     rd_en           ,  
-    output      [DWO-1:0]     rd_data         ,
+  input                     rd_clk          ,
+  input                     rd_en           ,  
+  output      [DWO-1:0]     rd_data         ,
 
-    output                    full            ,
-    output                    almost_full     ,
-    output      [AWO:0]       rd_data_cnt     ,
-    output                    empty           ,
-    output                    almost_empty    ,
-    output      [AWI:0]       wr_data_cnt
+  output                    full            ,
+  output                    almost_full     ,
+  output      [AWO:0]       rd_data_cnt     ,
+  output                    empty           ,
+  output                    almost_empty    ,
+  output      [AWI:0]       wr_data_cnt
 );
 
 localparam DEPTH_WR = {1'b1, {AWI{1'b0}}};
@@ -54,83 +54,81 @@ wire                    over_flag                   ;
 
 // write ptr
 always@(posedge wr_clk or negedge rst_n) begin
-    if(!rst_n) begin
-        wr_ptr <= 0;
-        wr_over <= 0;
+  if(!rst_n) begin
+    wr_ptr <= 0;
+    wr_over <= 0;
+  end
+  else begin
+    if(wr_en && !full) begin
+      {wr_over, wr_ptr} <= {wr_over, wr_ptr} + 1;
     end
-    else begin
-        if(wr_en && !full) begin
-            {wr_over, wr_ptr} <= {wr_over, wr_ptr} + 1;
-        end
-    end
+  end
 end
 
 // read ptr
 always@(posedge rd_clk or negedge rst_n) begin
-    if(!rst_n) begin
-        rd_ptr <= 0;
-        rd_over <= 0;
+  if(!rst_n) begin
+    rd_ptr <= 0;
+    rd_over <= 0;
+  end
+  else begin
+    if(rd_en && !empty) begin
+      {rd_over, rd_ptr} <= {rd_over, rd_ptr} + 1;
     end
-    else begin
-        if(rd_en && !empty) begin
-            {rd_over, rd_ptr} <= {rd_over, rd_ptr} + 1;
-        end
-    end
+  end
 end
 
 // convert write ptr to read clock
 
 Cnt_Cross_clock #(
-    .WIDTH(AWI+1)
+  .WIDTH(AWI+1)
 ) 
 Cnt_Cross_clock_wrptr_2rd ( 
-    .clk_in   ( wr_clk            ),
-    .cnt_in   ( {wr_over, wr_ptr} ),
-    .clk_out  ( rd_clk            ),
-    .cnt_out  ( {wr_over_2rd, wr_ptr_2rd} )
+  .clk_in   ( wr_clk            ),
+  .cnt_in   ( {wr_over, wr_ptr} ),
+  .clk_out  ( rd_clk            ),
+  .cnt_out  ( {wr_over_2rd, wr_ptr_2rd} )
 );
 
 // convert read ptr to write clock
 
 Cnt_Cross_clock #(
-    .WIDTH(AWO+1)
+  .WIDTH(AWO+1)
 ) 
 Cnt_Cross_clock_rdptr_2wr ( 
-    .clk_in   ( rd_clk            ),
-    .cnt_in   ( {rd_over, rd_ptr} ),
-    .clk_out  ( wr_clk            ),
-    .cnt_out  ( {rd_over_2wr, rd_ptr_2wr} )
+  .clk_in   ( rd_clk            ),
+  .cnt_in   ( {rd_over, rd_ptr} ),
+  .clk_out  ( wr_clk            ),
+  .cnt_out  ( {rd_over_2wr, rd_ptr_2wr} )
 );
 
 // full, empty signals .etc
 
 assign over_flag = wr_over == rd_over;
-assign wr_ptr_pre_1 = wr_ptr + 1;
 assign wr_ptr_pre_almost = wr_ptr + ALMOST_THRESH;
-assign rd_ptr_pre_1 = rd_ptr + 1;
 assign rd_ptr_pre_almost = rd_ptr + ALMOST_THRESH;
 
 genvar i;
 generate
-    if(IS_EXPAND) begin
-        assign empty        = (rd_ptr_pre_1<<EXPAND_BIT >= wr_ptr_2rd)          && over_flag    ;
-        assign almost_empty = (rd_ptr_pre_almost<<EXPAND_BIT >= wr_ptr_2rd)     && over_flag    ;
-        assign full         = (rd_ptr_2wr<<EXPAND_BIT <= wr_ptr_pre_1)          && !over_flag   ;
-        assign almost_full  = (rd_ptr_2wr<<EXPAND_BIT <= wr_ptr_pre_almost)     && !over_flag   ;
+  if(IS_EXPAND) begin
+    assign empty        = (rd_ptr >= wr_ptr_2rd>>EXPAND_BIT)                && over_flag    ;
+    assign almost_empty = (rd_ptr_pre_almost >= wr_ptr_2rd>>EXPAND_BIT)     && over_flag    ;
+    assign full         = (rd_ptr_2wr<<EXPAND_BIT <= wr_ptr)                && !over_flag   ;
+    assign almost_full  = (rd_ptr_2wr<<EXPAND_BIT <= wr_ptr_pre_almost)     && !over_flag   ;
 
-        assign rd_data_cnt = ({~over_flag, wr_ptr_2rd} >> EXPAND_BIT) - rd_ptr;
-        assign wr_data_cnt = {~over_flag, wr_ptr} - (rd_ptr_2wr<<EXPAND_BIT);
-    end
+    assign rd_data_cnt = ({~over_flag, wr_ptr_2rd} >> EXPAND_BIT) - rd_ptr;
+    assign wr_data_cnt = {~over_flag, wr_ptr} - (rd_ptr_2wr<<EXPAND_BIT);
+  end
 
-    else begin
-        assign empty        = (rd_ptr_pre_1 >= wr_ptr_2rd<<SHRINK_BIT)          && over_flag    ;
-        assign almost_empty = (rd_ptr_pre_almost >= wr_ptr_2rd<<SHRINK_BIT)     && over_flag    ;
-        assign full         = (rd_ptr_2wr <= wr_ptr_pre_1<<SHRINK_BIT)          && !over_flag   ;
-        assign almost_full  = (rd_ptr_2wr <= wr_ptr_pre_almost<<SHRINK_BIT)     && !over_flag   ;
+  else begin
+    assign empty        = (rd_ptr >= wr_ptr_2rd<<SHRINK_BIT)                && over_flag    ;
+    assign almost_empty = (rd_ptr_pre_almost >= wr_ptr_2rd<<SHRINK_BIT)     && over_flag    ;
+    assign full         = (rd_ptr_2wr>>SHRINK_BIT <= wr_ptr)                && !over_flag   ;
+    assign almost_full  = (rd_ptr_2wr>>SHRINK_BIT <= wr_ptr_pre_almost)     && !over_flag   ;
 
-        assign rd_data_cnt = ({~over_flag, wr_ptr_2rd}<<SHRINK_BIT) - rd_ptr;
-        assign wr_data_cnt = {~over_flag, wr_ptr} - (rd_ptr_2wr>>SHRINK_BIT);
-    end
+    assign rd_data_cnt = ({~over_flag, wr_ptr_2rd}<<SHRINK_BIT) - rd_ptr;
+    assign wr_data_cnt = {~over_flag, wr_ptr} - (rd_ptr_2wr>>SHRINK_BIT);
+  end
 endgenerate
 
 
@@ -139,20 +137,20 @@ endgenerate
 
 Ram_Async
 #( 
-    .DWI (DWI)  ,   // data width in
-    .AWI (AWI)  ,   // address width in
-    .DWO (DWO)  ,   // data width out
-    .AWO (AWO)      // address width out
+  .DWI (DWI)  ,   // data width in
+  .AWI (AWI)  ,   // address width in
+  .DWO (DWO)  ,   // data width out
+  .AWO (AWO)      // address width out
 )
 Ram_Async_inst
 (
-    .wr_clk    (wr_clk   ),     
-    .wr_en     (wr_en & !full),
-    .wr_addr   (wr_ptr   ),
-    .wr_data   (wr_data  ),
-    .rd_clk    (rd_clk   ),
-    .rd_addr   (rd_ptr   ),
-    .rd_data   (rd_data  )
+  .wr_clk    (wr_clk   ),     
+  .wr_en     (wr_en & !full),
+  .wr_addr   (wr_ptr   ),
+  .wr_data   (wr_data  ),
+  .rd_clk    (rd_clk   ),
+  .rd_addr   (rd_ptr   ),
+  .rd_data   (rd_data  )
 );
 
 
@@ -163,26 +161,26 @@ endmodule
 
 Fifo_Async
 #(
-    .DWI (8  ),   // data width in
-    .AWI (7  ),   // address width in
-    .DWO (16 ),   // data width out
-    .AWO (6  )    // address width out
+  .DWI (8  ),   // data width in
+  .AWI (7  ),   // address width in
+  .DWO (16 ),   // data width out
+  .AWO (6  )    // address width out
 )
 Fifo_Async_inst
 (   
-    .rst_n          (rst_n        ),
-    .wr_clk         (wr_clk       ),
-    .wr_en          (wr_en        ),            
-    .wr_data        (wr_data      ), // [DWI-1:0]
-    .rd_clk         (rd_clk       ),
-    .rd_en          (rd_en        ),  
-    .rd_data        (rd_data      ), // [DWO-1:0]
-    .full           (full         ),
-    .almost_full    (almost_full  ),
-    .wr_data_cnt    (wr_data_cnt  ), // [AWI-1:0]
-    .empty          (empty        ),
-    .almost_empty   (almost_empty ),
-    .rd_data_cnt    (rd_data_cnt  )  // [AWO-1:0]
+  .rst_n          (rst_n        ),
+  .wr_clk         (wr_clk       ),
+  .wr_en          (wr_en        ),            
+  .wr_data        (wr_data      ), // [DWI-1:0]
+  .rd_clk         (rd_clk       ),
+  .rd_en          (rd_en        ),  
+  .rd_data        (rd_data      ), // [DWO-1:0]
+  .full           (full         ),
+  .almost_full    (almost_full  ),
+  .wr_data_cnt    (wr_data_cnt  ), // [AWI-1:0]
+  .empty          (empty        ),
+  .almost_empty   (almost_empty ),
+  .rd_data_cnt    (rd_data_cnt  )  // [AWO-1:0]
 );
 
 */
